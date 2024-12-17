39c63fc440af77fe9651143c0a1359e48297457e
Aniket Raut <rautnaniket@gmail.com>
2024-12-17 13:00:00 +0530

    Modified Tailwind File

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        secondary: "#f43f5e",
+      },
+    },
+  },
+  plugins: [],
+};
