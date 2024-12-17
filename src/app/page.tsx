7c57e34333c2175fc6e4242dcfe6ba701acd60b9
Aniket Raut <rautnaniket@gmail.com>
2024-12-17 12:00:00 +0530

    Added All to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import Quiz from "@/components/Quiz";
+
 export default function Home() {
   return (
-    <main></main>
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-50 to-blue-100 p-4">
+      <div className="max-w-4xl w-full">
+        <Quiz />
+      </div>
+    </main>
   );
 }
