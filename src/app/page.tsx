8c9d391c37142bcf1748715e5c5ac99b2a742353
Aniket Raut <rautnaniket@gmail.com>
2024-12-18 12:00:00 +0530

    Modified Some Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Quiz from "@/components/Quiz";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-50 to-blue-100 p-4">
-      <div className="max-w-4xl w-full">
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-50 to-blue-100">
+      <div className=" w-full">
         <Quiz />
       </div>
     </main>
