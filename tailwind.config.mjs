f93e923079f73f42b0fa0d9710b8ee85627dbacd
Aniket Raut <rautnaniket@gmail.com>
2024-12-25 16:00:00 +0530

    Redesigned the results Page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,13 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       colors: {
         primary: "#4f46e5",
         secondary: "#f43f5e",
+        gold: "#FFD700",
       },
     },
   },
